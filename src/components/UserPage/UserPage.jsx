53dabbb5cf5fdc935a63245c5969185b5a90da21
searcilas <searcilas@unal.edu.co>
2024-06-05 23:19:00 -0500

    modificaciones

---
+++ src/components/UserPage/UserPage.jsx
@@ -0,0 +1,124 @@
+import React, { useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import './UserPage.css';
+
+const UserPage = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const userData = location.state?.user;
+  const [editable, setEditable] = useState(false);
+  const [editedUserData, setEditedUserData] = useState(userData);
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    navigate('/');
+  };
+
+  const handleEdit = () => {
+    setEditable(true);
+  };
+
+  const handleSave = () => {
+    // Aquí puedes enviar los datos editados a la API si es necesario
+    console.log('Datos editados:', editedUserData);
+    setEditable(false);
+    // Actualizar userData con los datos editados solo visualmente
+    location.state.user = { ...editedUserData };
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name === 'city') {
+      setEditedUserData({
+        ...editedUserData,
+        address: {
+          ...editedUserData.address,
+          city: value
+        }
+      });
+    } else {
+      setEditedUserData({
+        ...editedUserData,
+        [name]: value
+      });
+    }
+  };
+
+  return (
+    <div className="wrapper">
+      <h1>Perfil de Usuario</h1>
+      {editable ? (
+        <div>
+          <div className="input-box">
+            <input 
+              type="text" 
+              name="name" 
+              value={editedUserData.name} 
+              onChange={handleChange} 
+              placeholder="Nombre"
+            />
+          </div>
+          <div className="input-box">
+            <input 
+              type="text" 
+              name="username" 
+              value={editedUserData.username} 
+              onChange={handleChange} 
+              placeholder="Nombre de usuario"
+            />
+          </div>
+          <div className="input-box">
+            <input 
+              type="text" 
+              name="city" 
+              value={editedUserData.address.city} 
+              onChange={handleChange} 
+              placeholder="Ciudad"
+            />
+          </div>
+          <div className="input-box">
+            <input 
+              type="text" 
+              name="phone" 
+              value={editedUserData.phone} 
+              onChange={handleChange} 
+              placeholder="Teléfono"
+            />
+          </div>
+          <button onClick={handleSave} className="save-button">Guardar</button>
+          <button onClick={() => setEditable(false)} className="cancel-button">Cancelar</button>
+        </div>
+      ) : (
+        <div className="user-info">
+          <p><strong>Nombre:</strong> {userData.name}</p>
+          <p><strong>Correo electrónico:</strong> {userData.email}</p>
+          <p><strong>Nombre de usuario:</strong> {userData.username}</p>
+          <p><strong>Ciudad:</strong> {userData.address.city}</p>
+          <p><strong>Teléfono:</strong> {userData.phone}</p>
+        <div className="edit-button">
+          <button onClick={handleEdit} className="edit-button">Editar</button>
+        </div>
+          <button onClick={handleLogout} className="logout-button">Cerrar Sesión</button>
+        </div>
+      )}
+      
+    </div>
+  );
+};
+
+export default UserPage;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/components/WelcomePage/WelcomePage.js
+++ src/components/WelcomePage/WelcomePage.js
@@ -1,6 +1,6 @@
-// WelcomePage.js
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import './WelcomePage.css'; // Importamos el archivo de estilos
 
 const WelcomePage = () => {
   const location = useLocation();
@@ -15,28 +15,26 @@ const WelcomePage = () => {
   const handleProfile = () => {
     navigate('/perfil', { state: { user: userData } });
   };
-  
 
   return (
-    <div>
-      <div style={{ padding: '20px', backgroundColor: '#f2f2f2', borderRadius: '10px', maxWidth: '400px', margin: 'auto', textAlign: 'center' }}>
-        <h1 style={{ color: '#333', marginBottom: '20px' }}>¡Bienvenido!</h1>
+    <div className="wrapper">
+      <h1>¡Bienvenido!</h1>
+      <div className="user-info">
         {userData ? (
-          <div style={{ marginBottom: '20px' }}>
-            <p style={{ marginBottom: '10px' }}>Tu nombre es: <strong>{userData.name}</strong></p>
-            <p style={{ marginBottom: '10px' }}>Tu correo electrónico es: <strong>{userData.email}</strong></p>
-            <p style={{ marginBottom: '10px' }}>Tu nombre de usuario es: <strong>{userData.username}</strong></p>
-            <p style={{ marginBottom: '10px' }}>Tu ciudad es: <strong>{userData.address.city}</strong></p>
-            <p style={{ marginBottom: '40px' }}>Tu teléfono es: <strong>{userData.phone}</strong></p>
-          </div>
+          <>
+            <p><strong>Tu nombre es:</strong> {userData.name}</p>
+            <p><strong>Tu correo electrónico es:</strong> {userData.email}</p>
+            <p><strong>Tu nombre de usuario es:</strong> {userData.username}</p>
+            <p><strong>Tu ciudad es:</strong> {userData.address.city}</p>
+            <p><strong>Tu teléfono es:</strong> {userData.phone}</p>
+          </>
         ) : (
           <p>No se han proporcionado datos de usuario.</p>
         )}
-
-        <div>
-          <button onClick={handleLogout} style={{ backgroundColor: 'red', color: 'white', padding: '10px 20px', marginRight: '10px', borderRadius: '5px', border: 'none', cursor: 'pointer' }}>Cerrar sesión</button>
-          <button onClick={handleProfile} style={{ backgroundColor: '#007bff', color: 'white', padding: '10px 20px', borderRadius: '5px', border: 'none', cursor: 'pointer' }}>Perfil de Usuario</button>
-        </div>
+      </div>
+      <div className="button-container">
+        <button onClick={handleLogout} className="logout-button">Cerrar sesión</button>
+        <button onClick={handleProfile} className="profile-button">Perfil de Usuario</button>
       </div>
     </div>
   );
